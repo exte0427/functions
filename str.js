896437b21c8baebac0da525a5b5ff4139c7ce44f
ㄷㅌ <62054524+exte0427@users.noreply.github.com>
2020-05-22 23:13:11 +0900

    Update str.js

--- str.js
+++ str.js
@@ -19,3 +19,24 @@ String.prototype.compare = function(a){
     }
     return true;
 }
+String.prototype.data = function(a){
+    let t=a.split(">");
+    let str=this;
+    for(let i=0;i<t.length;i++){
+        const th=t[i].split("<")[0];
+        if(str.indexOf(th)!=-1){
+            str=str.replace(th,",");
+        }
+    }
+    const str2=[];
+    str.split(",").map(a=>{if(a!="" && a!=undefined){str2.push(a);}});
+    if(str2.length==1){
+        return str2[0];
+    }
+    else if(str2.length==0){
+        return 0;
+    }
+    else{
+        return str2;
+    }
+}
